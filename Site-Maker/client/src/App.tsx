dca9183433ec087ba30c0bc757d8f9a434cb5c0f
zizozizo123km <nnacar12@gmail.com>
2026-03-09 16:08:16 +0100

    Update Site-Maker/client/src/App.tsx

---
+++ Site-Maker/client/src/App.tsx
@@ -0,0 +1,34 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import NotFound from "@/pages/not-found";
+import Home from "./pages/Home";
+import Project from "./pages/Project";
+import AppLayout from "./components/layout/AppLayout";
+
+function Router() {
+  return (
+    <AppLayout>
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/project/:id" component={Project} />
+        <Route component={NotFound} />
+      </Switch>
+    </AppLayout>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Router />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
